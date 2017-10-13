0e91da1431c7dc9d72d8a4008e887b1e123cad70
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-13 17:20:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+
+  prime = 0 
+  for each in range(2..number)
+    if each % number == 0
+      return false
+      break
+    end
+  end
+  
+    
+      
+end
